079927f4f4e1083dc9360ee14e0027ffbdbe8398
AmitKumar <amit.dharmani12@gmail.com>
2024-05-09 19:01:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'myangularapp';
+  // constructor(){
+  //   console.log('----constructor----');
+  // }
+  @HostListener('click',['$event'])
+  showsdf (){
+    console.log('---hello---');
+  } 
+
+
 }
