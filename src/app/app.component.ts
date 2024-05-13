80232a37d79bb804d20818815dc7bc7d73c2a1a4
AmitKumar <amit.dharmani12@gmail.com>
2024-05-13 19:02:05 +0530

    send data child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,41 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { Book } from './book';
 
+class abc{
+  constructor(){
+    console.log('----abc------');
+  }
+  show(){
+    console.log('---show-----');
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,EmployeeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  // preserveWhitespaces:true,
+  // viewProviders:[abc,Book],
+  // encapsulation: ViewEncapsulation.None
 })
 
 export class AppComponent {
   title = 'myangularapp';
-  // constructor(){
+  // constructor(private _abc: abc, private _book: Book){
   //   console.log('----constructor----');
+  //   _abc.show()
   // }
-  @HostListener('click',['$event'])
-  showsdf (){
-    console.log('---hello---');
-  } 
-
+  // @HostListener('click',['$event'])
+  // showsdf (){
+  //   console.log('---hello---');
+  // }
+  Cdata:any;
+  // getdata(val:any){
+  //   this.Cdata = val
+  // }
 
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation,EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-employee',
   standalone: true,
   imports: [],
   templateUrl: './employee.component.html',
-  styleUrl: './employee.component.css'
+  styleUrl: './employee.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom,
+  inputs:['Pdata'],
+  outputs:['childevent']
 })
+
 export class EmployeeComponent {
+  Pdata:any;
+  
+  childevent = new EventEmitter();
+  senddata(val:any){
+    this.childevent.emit(val)
+  }
+
 
 }
